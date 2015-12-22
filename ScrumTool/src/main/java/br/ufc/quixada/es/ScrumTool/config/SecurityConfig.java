4606302ef7f7b755d82c4beb0930cc5c7893390e
Otavio Augusto <otavioaugustoes@gmail.com>
2015-12-22 16:12:25 -0300

    removendo a autenticação

--- ScrumTool/src/main/java/br/ufc/quixada/es/ScrumTool/config/SecurityConfig.java
+++ ScrumTool/src/main/java/br/ufc/quixada/es/ScrumTool/config/SecurityConfig.java
@@ -8,23 +8,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
-import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
-
-import br.ufc.quixada.es.ScrumTool.account.UserService;
 
 @Configuration
 @EnableWebMvcSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-    @Bean
-    public UserService userService() {
-        return new UserService();
-    }
+ 
 
-    @Bean
-    public TokenBasedRememberMeServices rememberMeServices() {
-        return new TokenBasedRememberMeServices("remember-me-key", userService());
-    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -33,32 +24,12 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .eraseCredentials(true)
-            .userDetailsService(userService())
-            .passwordEncoder(passwordEncoder());
+     
     }
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/favicon.ico", "/resources/**", "/signup").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/signin")
-                .permitAll()
-                .failureUrl("/signin?error=1")
-                .loginProcessingUrl("/authenticate")
-                .and()
-            .logout()
-                .logoutUrl("/logout")
-                .permitAll()
-                .logoutSuccessUrl("/signin?logout")
-                .and()
-            .rememberMe()
-                .rememberMeServices(rememberMeServices())
-                .key("remember-me-key");
+    	http
+        .httpBasic().disable();
     }
 }
